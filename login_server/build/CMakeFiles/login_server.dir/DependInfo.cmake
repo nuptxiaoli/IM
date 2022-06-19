521cd0aed3effcc28faeab1a02fe907c2b717eec
ubuntu <ubuntu@localhost.localdomain>
2022-06-19 19:35:52 +0800

    add login_server

---
+++ login_server/build/CMakeFiles/login_server.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/IM/login_server/HttpConn.cpp" "/home/ubuntu/IM/login_server/build/CMakeFiles/login_server.dir/HttpConn.cpp.o"
+  "/home/ubuntu/IM/login_server/LoginConn.cpp" "/home/ubuntu/IM/login_server/build/CMakeFiles/login_server.dir/LoginConn.cpp.o"
+  "/home/ubuntu/IM/login_server/ipparser.cpp" "/home/ubuntu/IM/login_server/build/CMakeFiles/login_server.dir/ipparser.cpp.o"
+  "/home/ubuntu/IM/login_server/login_server.cpp" "/home/ubuntu/IM/login_server/build/CMakeFiles/login_server.dir/login_server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AC_HAS_CRITICAL"
+  "AC_HAS_DEBUG"
+  "AC_HAS_ERROR"
+  "AC_HAS_INFO"
+  "AC_HAS_WARNING"
+  "LINUX_DAEMON"
+  "TIXML_USE_STL"
+  "_FILE_OFFSET_BITS=64"
+  "_REENTRANT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../../base"
+  "../../base/pb/protocol"
+  "../../base/jsoncpp"
+  "../../base/pb"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
