a97735f417de2e976d9087b1b330a38f02d8fc72
ubuntu <ubuntu@localhost.localdomain>
2022-06-19 15:53:05 +0800

    init code

---
+++ base/build/CMakeFiles/base.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/IM/base/Base64.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/Base64.cpp.o"
+  "/home/ubuntu/IM/base/BaseSocket.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/BaseSocket.cpp.o"
+  "/home/ubuntu/IM/base/Condition.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/Condition.cpp.o"
+  "/home/ubuntu/IM/base/ConfigFileReader.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/ConfigFileReader.cpp.o"
+  "/home/ubuntu/IM/base/EncDec.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/EncDec.cpp.o"
+  "/home/ubuntu/IM/base/EventDispatch.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/EventDispatch.cpp.o"
+  "/home/ubuntu/IM/base/HttpClient.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/HttpClient.cpp.o"
+  "/home/ubuntu/IM/base/HttpParserWrapper.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/HttpParserWrapper.cpp.o"
+  "/home/ubuntu/IM/base/ImPduBase.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/ImPduBase.cpp.o"
+  "/home/ubuntu/IM/base/Lock.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/Lock.cpp.o"
+  "/home/ubuntu/IM/base/ServInfo.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/ServInfo.cpp.o"
+  "/home/ubuntu/IM/base/Thread.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/Thread.cpp.o"
+  "/home/ubuntu/IM/base/ThreadPool.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/ThreadPool.cpp.o"
+  "/home/ubuntu/IM/base/TokenValidator.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/TokenValidator.cpp.o"
+  "/home/ubuntu/IM/base/UtilPdu.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/UtilPdu.cpp.o"
+  "/home/ubuntu/IM/base/http_parser.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/http_parser.cpp.o"
+  "/home/ubuntu/IM/base/im_conn_util.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/im_conn_util.cpp.o"
+  "/home/ubuntu/IM/base/imconn.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/imconn.cpp.o"
+  "/home/ubuntu/IM/base/jsoncpp/json_reader.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/jsoncpp/json_reader.cpp.o"
+  "/home/ubuntu/IM/base/jsoncpp/json_value.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/jsoncpp/json_value.cpp.o"
+  "/home/ubuntu/IM/base/jsoncpp/json_writer.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/jsoncpp/json_writer.cpp.o"
+  "/home/ubuntu/IM/base/log4z.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/log4z.cpp.o"
+  "/home/ubuntu/IM/base/netlib.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/netlib.cpp.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.BaseDefine.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.BaseDefine.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.Buddy.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.Buddy.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.File.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.File.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.Group.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.Group.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.Login.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.Login.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.Message.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.Message.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.Other.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.Other.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.Server.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.Server.pb.cc.o"
+  "/home/ubuntu/IM/base/pb/protocol/IM.SwitchService.pb.cc" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/pb/protocol/IM.SwitchService.pb.cc.o"
+  "/home/ubuntu/IM/base/util.cpp" "/home/ubuntu/IM/base/build/CMakeFiles/base.dir/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AC_HAS_CRITICAL"
+  "AC_HAS_DEBUG"
+  "AC_HAS_ERROR"
+  "AC_HAS_INFO"
+  "AC_HAS_WARNING"
+  "LINUX_DAEMON"
+  "TIXML_USE_STL"
+  "_FILE_OFFSET_BITS=64"
+  "_REENTRANT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../.."
+  ".././jsoncpp"
+  ".././pb"
+  ".././pb/protocol"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
